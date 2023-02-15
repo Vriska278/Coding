60dbf8a6af72de6aabc2db3f44894dec0ed55f59
Vriska278 <vriska278@gmail.com>
2023-02-15 14:13:39 -0600

    adding assignments and updating old ones

--- Stack_1_RAW/scratch_code.js
+++ Stack_1_RAW/scratch_code.js
@@ -1,12 +1,12 @@
-function abc(number){
-    var sum = 0;
-    for(let i=0; i<=number; i++){
-        sum= sum + i
-        //console.log(i)
+var countPositives = 0;
+var numbers = [3, 4, -2, 7, 16, -8, 0];
+
+    function positiveArray(numbers){
+        for(var i=0; i<numbers.length; i++)
+            if(numbers[i]>0){
+                countPositives += 1
+            }
+        return countPositives
     }
-    console.log(i)
-    //console.log(sum);
-    return sum+10;
-}
-var output = abc(2);
-console.log(output);
+positiveArray(numbers)
+console.log("there are " + countPositives + " positive values");
