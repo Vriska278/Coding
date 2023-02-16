d11861283e9e905c684ea14fa3ea575f2d3079c2
Vriska278 <vriska278@gmail.com>
2023-02-16 13:26:08 -0600

    first commit User_Card

--- Stack_1_RAW/scratch_code.js
+++ Stack_1_RAW/scratch_code.js
@@ -1,12 +1,11 @@
-var countPositives = 0;
-var numbers = [3, 4, -2, 7, 16, -8, 0];
-
-    function positiveArray(numbers){
-        for(var i=0; i<numbers.length; i++)
-            if(numbers[i]>0){
-                countPositives += 1
-            }
-        return countPositives
-    }
-positiveArray(numbers)
-console.log("there are " + countPositives + " positive values");
+var x = 5;
+    
+function addToX(amount) {
+    return x + amount;
+    console.log("hello there");
+}
+    
+console.log(x);
+var result = addToX(-10);
+console.log(result);
+console.log(x);
