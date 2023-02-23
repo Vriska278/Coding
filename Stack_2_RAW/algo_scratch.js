cae2ef84a518c9b4d9ec6e317438917fd652ac68
Vriska278 <vriska278@gmail.com>
2023-02-23 16:21:39 -0600

    second commit

--- Stack_2_RAW/algo_scratch.js
+++ Stack_2_RAW/algo_scratch.js
@@ -39,29 +39,57 @@
 
 
 
-var pokémon = [
-  { "id": 1,   "name": "Bulbasaur",  "types": ["poison", "grass"] },
-  { "id": 5,   "name": "Charmeleon", "types": ["fire"] },
-  { "id": 9,   "name": "Blastoise",  "types": ["water"] },
-  { "id": 12,  "name": "Butterfree", "types": ["bug", "flying"] },
-  { "id": 16,  "name": "Pidgey",     "types": ["normal", "flying"] },
-  { "id": 23,  "name": "Ekans",      "types": ["poison"] },
-  { "id": 24,  "name": "Arbok",      "types": ["poison"] },
-  { "id": 25,  "name": "Pikachu",    "types": ["electric"] },
-  { "id": 37,  "name": "Vulpix",     "types": ["fire"] },
-  { "id": 52,  "name": "Meowth",     "types": ["normal"] },
-  { "id": 63,  "name": "Abra",       "types": ["psychic"] },
-  { "id": 67,  "name": "Machamp",    "types": ["fighting"] },
-  { "id": 72,  "name": "Tentacool",  "types": ["water", "poison"] },
-  { "id": 74,  "name": "Geodude",    "types": ["rock", "ground"] },
-  { "id": 87,  "name": "Dewgong",    "types": ["water", "ice"] },
-  { "id": 98,  "name": "Krabby",     "types": ["water"] },
-  { "id": 115, "name": "Kangaskhan", "types": ["normal"] },
-  { "id": 122, "name": "Mr. Mime",   "types": ["psychic"] },
-  { "id": 133, "name": "Eevee",      "types": ["normal"] },
-  { "id": 144, "name": "Articuno",   "types": ["ice", "flying"] },
-  { "id": 145, "name": "Zapdos",     "types": ["electric", "flying"] },
-  { "id": 146, "name": "Moltres",    "types": ["fire", "flying"] },
-  { "id": 148, "name": "Dragonair",  "types": ["dragon"] }
-];
+// var pokémon = [
+//   { "id": 1,   "name": "Bulbasaur",  "types": ["poison", "grass"] },
+//   { "id": 5,   "name": "Charmeleon", "types": ["fire"] },
+//   { "id": 9,   "name": "Blastoise",  "types": ["water"] },
+//   { "id": 12,  "name": "Butterfree", "types": ["bug", "flying"] },
+//   { "id": 16,  "name": "Pidgey",     "types": ["normal", "flying"] },
+//   { "id": 23,  "name": "Ekans",      "types": ["poison"] },
+//   { "id": 24,  "name": "Arbok",      "types": ["poison"] },
+//   { "id": 25,  "name": "Pikachu",    "types": ["electric"] },
+//   { "id": 37,  "name": "Vulpix",     "types": ["fire"] },
+//   { "id": 52,  "name": "Meowth",     "types": ["normal"] },
+//   { "id": 63,  "name": "Abra",       "types": ["psychic"] },
+//   { "id": 67,  "name": "Machamp",    "types": ["fighting"] },
+//   { "id": 72,  "name": "Tentacool",  "types": ["water", "poison"] },
+//   { "id": 74,  "name": "Geodude",    "types": ["rock", "ground"] },
+//   { "id": 87,  "name": "Dewgong",    "types": ["water", "ice"] },
+//   { "id": 98,  "name": "Krabby",     "types": ["water"] },
+//   { "id": 115, "name": "Kangaskhan", "types": ["normal"] },
+//   { "id": 122, "name": "Mr. Mime",   "types": ["psychic"] },
+//   { "id": 133, "name": "Eevee",      "types": ["normal"] },
+//   { "id": 144, "name": "Articuno",   "types": ["ice", "flying"] },
+//   { "id": 145, "name": "Zapdos",     "types": ["electric", "flying"] },
+//   { "id": 146, "name": "Moltres",    "types": ["fire", "flying"] },
+//   { "id": 148, "name": "Dragonair",  "types": ["dragon"] }
+// ];
 
+
+// var arr2d = [ [2, 5, 8],
+//               [3, 6, 1],
+//               [5, 7, 7] ]
+// function checkValue(){
+//   for(var i=0; i<arr2d.length; i++){
+//     for(var j=0; j<arr2d[i].length; j++){
+//       if(arr2d[i][j]==10){
+//         console.log(arr2d[i][j])
+//         return true
+//       }
+//     }
+//   }
+//   console.log("false")
+//   }
+// checkValue();
+
+// function flatten(arr2d){
+//   var flat=[]
+//   for(var i=0; i<arr2d.length; i++){
+//     for(var j=0; j<arr2d[i].length; j++){
+//       flat.push(arr2d[i][j])
+//     }
+//   }
+//       return flat;
+// }
+// var result = flatten( [ [2, 5, 8], [3, 6, 1], [5, 7, 7] ] );
+// console.log(result);
