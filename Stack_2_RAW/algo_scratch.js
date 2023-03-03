dbd3ff24db82d1d3180b585345616ef9e48e1d94
Vriska278 <vriska278@gmail.com>
2023-03-03 17:29:40 -0600

    add new algo

--- Stack_2_RAW/algo_scratch.js
+++ Stack_2_RAW/algo_scratch.js
@@ -112,36 +112,36 @@ Expected Output: [3, 4, 1, 2]
 
 */
 
-const arr = [1, 2, 3, 4];
+// const arr = [1, 2, 3, 4];
 
-function rotateArray(arr, shiftBy) {
-    let newArr = [4,1,2,3];
-    if (shiftBy > 0) {
-        for (let i = arr.length - shiftBy; i < arr.length; i++) {
-            newArr.push(arr[i]);
-        }
-        for (let i = 0; i < arr.length - shiftBy; i++) {
-            newArr.push(arr[i]);
-        }
-    }
-    else if(shiftBy < 0){
-        for(let i = -shiftBy; i < arr.length; i++){
-            newArr.push(arr[i]);
-        }
-        for(let i = 0; i < -shiftBy; i++){
-            newArr.push(arr[i]);
-        }
-    }
-    else{
-        return arr;
-    }
+// function rotateArray(arr, shiftBy) {
+//     let newArr = [4,1,2,3];
+//     if (shiftBy > 0) {
+//         for (let i = arr.length - shiftBy; i < arr.length; i++) {
+//             newArr.push(arr[i]);
+//         }
+//         for (let i = 0; i < arr.length - shiftBy; i++) {
+//             newArr.push(arr[i]);
+//         }
+//     }
+//     else if(shiftBy < 0){
+//         for(let i = -shiftBy; i < arr.length; i++){
+//             newArr.push(arr[i]);
+//         }
+//         for(let i = 0; i < -shiftBy; i++){
+//             newArr.push(arr[i]);
+//         }
+//     }
+//     else{
+//         return arr;
+//     }
 
 
-    return newArr; // Can also do this in place and return arr if you want an additional challenge!
-}
+//     return newArr; // Can also do this in place and return arr if you want an additional challenge!
+// }
 
-console.log(rotateArray(arr, 1))
-console.log(rotateArray(arr, -4))
+// console.log(rotateArray(arr, 1))
+// console.log(rotateArray(arr, -4))
 
 // Bonus Challenge: Allow the ShiftBy to be a negative value to shift left, or a positive value to shift right.
 //[2,3,4,1]  -1
@@ -159,48 +159,107 @@ console.log(rotateArray(arr, -4))
 */
 
 
-function removeIndex(arr, index){
-    if(index >= arr.length || index < 0){
-        return null;
-    }
-    temp = arr[index];
-    for(let i = index; i < arr.length - 1; i++){
-        arr[i] = arr[i + 1];
-    }
-    arr.pop();    
+// function removeIndex(arr, index){
+//     if(index >= arr.length || index < 0){
+//         return null;
+//     }
+//     temp = arr[index];
+//     for(let i = index; i < arr.length - 1; i++){
+//         arr[i] = arr[i + 1];
+//     }
+//     arr.pop();    
     
-    return temp;
-}
+//     return temp;
+// }
 
-const arr1 = ["a", "b", "c", "d", "e"]; 
-const removeIdx1 = 1;
-const expected1 = "b";
-// after function call, arr1 should be:
-const arr1Expected = ["a", "c", "d", "e"];
+// const arr1 = ["a", "b", "c", "d", "e"]; 
+// const removeIdx1 = 1;
+// const expected1 = "b";
+// const arr1Expected = ["a", "c", "d", "e"];
 
-console.log(removeIndex(arr1, 1));
-console.log(arr1);
+// console.log(removeIndex(arr1, 1));
+// console.log(arr1);
 
-const arr2 = ["a", "b", "c"];
-const removeIdx2 = 3;
-const expected2 = null;
-const arr2Expected = ["a", "b", "c"];
+// const arr2 = ["a", "b", "c"];
+// const removeIdx2 = 3;
+// const expected2 = null;
+// const arr2Expected = ["a", "b", "c"];
 
-console.log(removeIndex(arr2, 3));
-console.log(arr2);
+// console.log(removeIndex(arr2, 3));
+// console.log(arr2);
 
-const arr3 = ["a", "b", "c"];
-const removeIdx3 = -3;
-const expected3 = null;
-const arr3Expected = ["a", "b", "c"];
+// const arr3 = ["a", "b", "c"];
+// const removeIdx3 = -3;
+// const expected3 = null;
+// const arr3Expected = ["a", "b", "c"];
 
-console.log(removeIndex(arr3, -3));
-console.log(arr3);
+// console.log(removeIndex(arr3, -3));
+// console.log(arr3);
 
-const arr4 = ["cake", "pie", "cookies", "vegetables", "candy"];
-const removeIdx4 = 3;
-const expected4 = "vegetables";
-const arr4Expected =  ["cake", "pie", "cookies", "candy"];
+// const arr4 = ["cake", "pie", "cookies", "vegetables", "candy"];
+// const removeIdx4 = 3;
+// const expected4 = "vegetables";
+// const arr4Expected =  ["cake", "pie", "cookies", "candy"];
 
-console.log(removeIndex(arr4, 3));
-console.log(arr4);
+// console.log(removeIndex(arr4, 3));
+// console.log(arr4);
+
+// var matches = [
+// ["HTML", "C#"],
+// ["C#", "Python"],
+// ["Python", "HTML"]
+// ]
+
+// var results = [0,0,1]
+
+// function tournament(matches, results){
+//     var maxScore = 0
+//     var tournamentWinner = " "
+//     var score = []
+//     var winnerInArray = false
+//     var loserInArray = false
+//     for(var i=0; i < matches.length; i++){
+//         if(results[i]==0){
+//             var winner = matches[i][1]
+//             var loser = matches[i][0]
+//         }
+//         else if(results[i]==1){
+//             var winner = matches[i][0]
+//             var loser = matches[i][1]
+//         }
+//         for(j = 0; j < score.length; j++){
+//             if(score[j][0]==winner){
+//                 winnerInArray = true
+//             }
+//             if(score[j][0]==loser){
+//                 loserInArray = true
+//             }
+//         }
+//         if(!winnerInArray){
+//             score.push([winner,0])
+//         }
+//         if(!loserInArray){
+//             score.push([loser,0])
+//         }
+//         for(j = 0; j < score.length; j++){
+//             if(score[j][0]==winner){
+//                 score[j][1]+=3
+//             }
+//             if(score[j][0]==loser){
+//                 score[j][1]+=0
+//             }
+//         }
+
+//         console.log(winner+" beats " +loser )
+//     }
+//     for(h=0; h<score.length;h++){
+//         console.log(''+score[h][0]+' - '+score[h][1]+'')
+//         if(score[h][1]>maxScore){
+//             maxScore = score[h][1]
+//             tournamentWinner = "Tournament Winner: " + score[h][0] + ' ' + maxScore
+//         }
+//     }
+//     return tournamentWinner
+// }
+
+// console.log(tournament(matches, results));
